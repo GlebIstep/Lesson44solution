52acf62338333866f3e016b73cccbe35e700587b
GlebIstep <glebasik52525@gmail.com>
2026-02-25 17:25:22 +0300

    Добавьте файлы проекта.

---
+++ task01/main.cpp
@@ -0,0 +1,20 @@
+#include "util.h"
+
+int main() {
+	int array[DEFAULT_SIZE];
+	int size;
+
+	do {
+		system("cls");
+		cout << "Input size of array: ";
+		cin >> size;
+	} while (size <= 0);
+
+
+	init_random(array, size, 0, 100);
+
+	print("Array:\n");
+	print(convert(array, size));
+
+	return 0;
+}
